c7605e581b9f0ca574adcb026734d466581c5d95
hab56ur9 <hab5bur9@gmail.com>
2025-03-12 21:05:32 +0900

    feat: findAllByUserID 작성

--- pkg/feedsvc/domain/repository.go
+++ pkg/feedsvc/domain/repository.go
@@ -1,4 +1,5 @@
 package domain
 
 type FeedRepository interface {
+	FindAllByUserID(id uint16) ([]Feed, error)
 }
--- pkg/feedsvc/infra/rdb.go
+++ pkg/feedsvc/infra/rdb.go
@@ -13,3 +13,33 @@ func NewFeedMySQLRepository(db *gorm.DB) domain.FeedRepository {
 type mysqlRepo struct {
 	db *gorm.DB
 }
+
+type Feed struct {
+	ID      uint16 `gorm:"primaryKey"`
+	UserID  uint16
+	Title   string
+	Content string
+	ImgURL  []string
+}
+
+func toEntities(records []Feed) []domain.Feed {
+	entities := make([]domain.Feed, 0, len(records))
+	for _, r := range records {
+		entities = append(entities, domain.Feed{
+			ID:      r.ID,
+			UserID:  r.UserID,
+			Title:   r.Title,
+			Content: r.Content,
+			ImgURL:  r.ImgURL,
+		})
+	}
+	return entities
+}
+
+func (r *mysqlRepo) FindAllByUserID(id uint16) ([]domain.Feed, error) {
+	var records []Feed
+	if err := r.db.Where("user_id = ?", id).Find(&records).Error; err != nil {
+		return nil, err
+	}
+	return toEntities(records), nil
+}
